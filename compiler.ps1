945b093da08e87c35e289ded8bdd42b041f81811
mocha <79212970+mochawoof@users.noreply.github.com>
2023-10-24 14:57:00 -0400

    1.1 update
    added support for passing compile arguments directly to the script

--- compiler.ps1
+++ compiler.ps1
@@ -1,5 +1,13 @@
 # check README.txt for info
 $conf = Get-Content -Path .config | ConvertFrom-Json
+if ($args[0]) {
+	$conf = [PSCustomObject]@{
+		source = $args[0]
+		namespace = $args[1]
+		class = $args[2]
+		args = $args[3]
+	}
+}
 $r = -join ((65..90) + (97..122) | Get-Random -Count 8 | % {[char]$_})
 $c = ([regex]$conf.namespace).replace((Get-Content $conf.source), $r, 1) | Out-String
 Add-Type -TypeDefinition $c -Language CSharp
